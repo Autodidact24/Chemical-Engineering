7c91b21ae9fcf0b4a5e8d2fb477c066fdcff9084
Shubham Singh Tomar <shubham24@Ubuntu.(none)>
2013-10-19 00:21:28 +0530

    Modeling a simple Batch Distillation problem

--- Reactive_distillation/Reactive_distillation.py
+++ Reactive_distillation/Reactive_distillation.py
@@ -3,12 +3,15 @@
 
 # Modeling Reactive distillation
 
+# Starting out - Simple batch distillation problem
+
 
 from scipy import *
 from scipy.integrate import odeint
 from pylab import *
 import matplotlib.pyplot as plt
 
+# Plugging-in contant values
 D = 10
 a = 2.41
 W_int = 100
@@ -16,6 +19,7 @@ x_w_int = 0.5
 
 t = range(0, 6, 1)
 
+# Defining function for integration
 def batch(x_w, t):
 	W_t = -D*t + W_int
 	y = a*x_w/ (1.0 + x_w*(a-1.0))
@@ -25,11 +29,11 @@ def batch(x_w, t):
 
 x_w_sol = odeint(batch, x_w_int, t)
 
+# Plotting the solution
 figure()
 plot(t, x_w_sol, '-o')
 xlabel('Time, hours')
 ylabel('x_w')
 grid()
 plt.show()
-
 
